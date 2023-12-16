504a8e864baabe381fa258e31738ad355eb6e6d0
Tushar Bapu Adling <110562490+tushar242001@users.noreply.github.com>
2023-12-16 13:03:45 +0530

    Create Stack_Implementation.cpp

---
+++ Stack_Implementation.cpp
@@ -0,0 +1,17 @@
+/*   ____________________Stack______________________________
+Stack : A stack is a linear data structure in which the insertion of a new element and 
+       removal of an existing element takes place at the same end represented as the top of the stack.
+
+The functions associated with stack are:
+
+empty() – Returns whether the stack is empty – Time Complexity : O(1) 
+size() – Returns the size of the stack – Time Complexity : O(1) 
+top() – Returns a reference to the top most element of the stack – Time Complexity : O(1) 
+push(g) – Adds the element ‘g’ at the top of the stack – Time Complexity : O(1) 
+pop() – Deletes the most recent entered element of the stack – Time Complexity : O(1)     */
+
+// Algorithm for push:
+
+// Implementation of Stack using Area .
+
+
